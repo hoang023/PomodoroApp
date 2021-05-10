41b8dac99b4028dc745a880fa470aac01b7bde45
HoangLong-212 <longtran2132000@gmail.com>
2021-05-11 04:58:16 +0700

    default Screen

---
+++ app/src/main/java/com/example/pomodoro/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.pomodoro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.Display;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
+
+
