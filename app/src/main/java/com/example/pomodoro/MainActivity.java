0fa989cb2dde62a0109007a45bff7b6a2f77b49b
HoangLong-212 <longtran2132000@gmail.com>
2021-05-13 19:15:54 +0700

    countdown time, skipbutton

--- app/src/main/java/com/example/pomodoro/MainActivity.java
+++ app/src/main/java/com/example/pomodoro/MainActivity.java
@@ -2,78 +2,145 @@ package com.example.pomodoro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
-import android.graphics.Point;
 import android.os.Bundle;
-import android.transition.Transition;
+import android.os.CountDownTimer;
 import android.transition.TransitionManager;
 import android.util.Log;
-import android.view.Display;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    private static final long startTime = 60000;
     private Button play;
-    private Button play_b;
-    private Button pause_b;
     private ViewGroup pause;
-    private Button stopb;
     private Button skipb;
     private ViewGroup transitionsContainer;
     private boolean visible;
+
+    private ImageView countdownButton;
+    private TextView countdownText;
+    private Button countdownStop;
+    private CountDownTimer countDownTimer;
+    private long timeLeftInMilliseconds = startTime;
+    private boolean timerRunning;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("onCreate","Executed");
         SetupUIView();
         play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("aaa","Executed");
-                TransitionManager.beginDelayedTransition(transitionsContainer);
-                play.setVisibility(View.GONE);
-                pause.setVisibility(View.VISIBLE);
-                stopb.setVisibility(View.VISIBLE);
-                Animation animation = AnimationUtils.loadAnimation(MainActivity.this,R.anim.righttoleft);
-                pause.startAnimation(animation);
-                Animation animation1 = AnimationUtils.loadAnimation(MainActivity.this,R.anim.lefttoright);
-                stopb.startAnimation(animation1);
+                if(timeLeftInMilliseconds !=0) {
+                    TransitionManager.beginDelayedTransition(transitionsContainer);
+                    play.setVisibility(View.GONE);
+                    pause.setVisibility(View.VISIBLE);
+                    countdownStop.setVisibility(View.VISIBLE);
+                    Animation animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.righttoleft);
+                    pause.startAnimation(animation);
+                    Animation animation1 = AnimationUtils.loadAnimation(MainActivity.this, R.anim.lefttoright);
+                    countdownStop.startAnimation(animation1);
+                    startTimer();
+                }
+                else {
+                    Toast.makeText(MainActivity.this,"you have to set the time",Toast.LENGTH_SHORT).show();
+                }
             }
         });
         skipb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TransitionManager.beginDelayedTransition(transitionsContainer);
+                visible = false;
                 play.setVisibility(View.VISIBLE);
                 pause.setVisibility(View.GONE);
-                stopb.setVisibility(View.GONE);
+                countdownStop.setVisibility(View.GONE);
+                countdownButton.setImageResource(R.drawable.pause);
+                timeLeftInMilliseconds = 0;
+                countDownTimer.cancel();
+                updateTimer();
+                timerRunning = false;
+                //countDownTimer.cancel();
+                //timerRunning = false;
             }
         });
-        pause.setOnClickListener(new View.OnClickListener() {
+
+        countdownButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TransitionManager.beginDelayedTransition(transitionsContainer);
-                Log.d("Debug pause", "Executed");
-                visible =!visible;
-                play_b.setVisibility(visible? View.VISIBLE : View.GONE);
-                pause_b.setVisibility(visible? View.GONE : View.VISIBLE);
+                if(timerRunning){
+                    pauseTimer();
+                }
+                else{
+                    startTimer();
+                }
             }
         });
+        countdownStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetTimer();
+            }
+        });
+        updateTimer();
     }
+
+    private void resetTimer() {
+        timeLeftInMilliseconds = startTime;
+        updateTimer();
+        pauseTimer();
+
+    }
+
+    private void startTimer() {
+        countDownTimer = new CountDownTimer(timeLeftInMilliseconds, 500) {
+            @Override
+            public void onTick(long millsUntilFinished) {
+                timeLeftInMilliseconds = millsUntilFinished;
+                updateTimer();
+            }
+            @Override
+            public void onFinish() {
+                countdownText.setText("00:00");
+                timerRunning = false;
+                countdownButton.setImageResource(R.drawable.playbutton);
+            }
+        }.start();
+        timerRunning = true;
+        countdownButton.setImageResource(R.drawable.pause);
+    }
+
+    private void pauseTimer() {
+        countDownTimer.cancel();
+        countdownButton.setImageResource(R.drawable.playbutton);
+        timerRunning = false;
+    }
+    private void updateTimer() {
+        int minutes = (int) (timeLeftInMilliseconds / 1000) / 60;
+        int seconds = (int) (timeLeftInMilliseconds / 1000) % 60;
+
+
+        String timeLefText = String.format(Locale.getDefault(), "%02d:%02d", minutes , seconds);
+        countdownText.setText(timeLefText);
+    }
+
     private void SetupUIView() {
         transitionsContainer = (ViewGroup) findViewById(R.id.relative);
         play = (Button) findViewById(R.id.play);
-        play_b = (Button) findViewById(R.id.playbutton);
-        pause_b = (Button) findViewById(R.id.pausebutton);
-        stopb = (Button) findViewById(R.id.stop);
+        countdownButton = findViewById(R.id.pausebutton);
         pause = (ViewGroup) findViewById(R.id.pause);
         skipb = (Button) findViewById(R.id.skipbutton);
+        countdownText = (TextView) findViewById(R.id.time);
+        countdownStop = findViewById(R.id.stop);
     }
 }
