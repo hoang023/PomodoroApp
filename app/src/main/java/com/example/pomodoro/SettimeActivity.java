53ec6e65121d382a73db3862ade8f88c583d0587
UIT19520358 <19520358@gm.uit.edu.vn>
2021-05-15 01:00:49 +0700

    screen exchaging

---
+++ app/src/main/java/com/example/pomodoro/SettimeActivity.java
@@ -0,0 +1,29 @@
+package com.example.pomodoro;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettimeActivity extends AppCompatActivity {
+
+    private Button tasksbutton;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settime);
+        tasksbutton = (Button) findViewById(R.id.tasksbutton);
+
+        tasksbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SettimeActivity.this,TaskActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
