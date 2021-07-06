f671636c0e7a33fdfc31821f2b412b3162bb2b61
UIT19520358 <19520358@gm.uit.edu.vn>
2021-07-06 11:40:59 +0700

    1.2.0

--- app/src/main/java/com/example/pomodoro/TasksFunctions/TasksAdd.java
+++ app/src/main/java/com/example/pomodoro/TasksFunctions/TasksAdd.java
@@ -28,7 +28,6 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.firestore.DocumentReference;
 
 import org.jetbrains.annotations.NotNull;
 
---
+++ app/src/main/java/com/example/pomodoro/TasksFunctions/TouchHelper.java
@@ -0,0 +1,64 @@
+package com.example.pomodoro.TasksFunctions;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Canvas;
+import android.graphics.Color;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.pomodoro.R;
+
+import org.jetbrains.annotations.NotNull;
+
+import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator;
+
+public class TouchHelper extends ItemTouchHelper.SimpleCallback {
+    private RecyclerView_Config.DataAdapter adapter;
+
+    public TouchHelper(RecyclerView_Config.DataAdapter adapter) {
+        super(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT);
+        this.adapter = adapter;
+    }
+
+    @Override
+    public boolean onMove(@NonNull @NotNull RecyclerView recyclerView, @NonNull @NotNull RecyclerView.ViewHolder viewHolder, @NonNull @NotNull RecyclerView.ViewHolder target) {
+        return false;
+    }
+
+    @Override
+    public void onSwiped(@NonNull @NotNull RecyclerView.ViewHolder viewHolder, int direction) {
+        final int position = viewHolder.getAdapterPosition();
+        if (direction == ItemTouchHelper.RIGHT){
+            AlertDialog.Builder builder = new AlertDialog.Builder(adapter.getContext());
+            builder.setMessage("Are you sure?")
+                    .setTitle("Delete Task")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            adapter.deleteData(position);
+                        }
+                    }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    adapter.notifyItemChanged(position);
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onChildDraw(@NonNull @NotNull Canvas c, @NonNull @NotNull RecyclerView recyclerView, @NonNull @NotNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+        new RecyclerViewSwipeDecorator.Builder(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+                .addSwipeRightActionIcon(R.drawable.ic_baseline_delete_24)
+                .addSwipeRightBackgroundColor(Color.RED)
+                .addSwipeLeftActionIcon(R.drawable.ic_baseline_edit_24)
+                .addSwipeLeftBackgroundColor(Color.BLUE)
+                .create()
+                .decorate();
+        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+    }
+}
